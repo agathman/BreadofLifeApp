7eeb137909b975f4eaf5019d3fd4467e1159711a
agathman <agathman@outlook.com>
2021-09-28 16:40:19 -0500

    2nd Commit

--- myapp/index.js
+++ myapp/index.js
@@ -10,29 +10,34 @@ app.use(cors());
 app.use(express.urlencoded({extended:false }));
 app.use(express.json());
 
+//DB var name
 let projectDB;
+//Connection string to DB
 let connectionString = 'mongodb://localhost:27017/projectdb'
 
+//Connecting to MongoDB
 MongoClient.connect (
     connectionString,
     { useNewUrlParser: true, useUnifiedTopology: true },
     function (err, client) {
         projectDB = client.db();
-        app.listen(port, () => console.log('Listening on Port ${port}!'));
+        app.listen(port, () => console.log('Listening on Port ' + port ));
     }
 );
 
+//Viewing all documents in the client collection
 app.get('/client', (req, res) => {
     projectDB.collection('client').find().toArray(function (err, items) {
         res.json(items);
     });
 });
 
+//Adding a client to the collection
 app.post('/client', (req, res) => {
 
-    const client = req.body;
+    const newClient = req.body;
 
-    console.log(client);
+    console.log(newClient);
     
         projectDB.collection('client').insertOne(req.body, function (err, info) {
         console.log(err);
@@ -40,3 +45,5 @@ app.post('/client', (req, res) => {
         res.send('Client is added to DB');
     });
 });
+
+
