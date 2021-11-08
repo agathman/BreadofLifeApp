ffbb237988e5a87cc06c99819133cc84a7b6f946
kylej555 <60441635+kylej555@users.noreply.github.com>
2021-11-08 15:45:58 -0600

    Non-functional nav bar

--- bol-management-app/src/App.vue
+++ bol-management-app/src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <!-- Non-functional Nav Bar -->
+  <nav class="navbar navbar-expand navbar-light bg-light">
+      <div class="container">
+      <a class="navbar-brand" href="/">Event Management App</a>
+        <div class="collapse navbar-collapse" >
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/createClient"
+                >Create Client</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/createEvent"
+                >Create Event</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/viewClients"
+                >View Clients</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/viewEvent"
+                >View Event</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Router view -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
