0d511bfa0fd2b730a95766322ba9cd434cb1ee54
kylej555 <60441635+kylej555@users.noreply.github.com>
2021-11-09 13:39:36 -0600

    Working router

--- bol-management-app/src/components/HelloWorld.vue
+++ bol-management-app/src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <b-alert show>Default Alert</b-alert>
+    
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -29,5 +32,32 @@
     </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
 
 
--- bol-management-app/src/main.js
+++ bol-management-app/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//import router from './router'
+import router from './router'
 
 const app = createApp(App)
 //Not sure how to use router yet
-//app.use(router)
+app.use(router)
 app.mount('#app')
