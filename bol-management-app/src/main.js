9ea282520a5b416660191c1e8ce9776c96899b73
kylej555 <60441635+kylej555@users.noreply.github.com>
2021-11-09 17:12:59 -0600

    fixed bootstrap

--- bol-management-app/src/main.js
+++ bol-management-app/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import BootstrapVue3 from 'bootstrap-vue-3'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 const app = createApp(App)
 //Not sure how to use router yet
 app.use(BootstrapVue3)
