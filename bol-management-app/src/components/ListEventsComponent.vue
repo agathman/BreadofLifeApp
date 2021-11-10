19d63469362712802d0d8076700d5b8dd3b410da
kylej555 <60441635+kylej555@users.noreply.github.com>
2021-11-09 22:56:00 -0600

    working list views

--- bol-management-app/src/components/ListEventsComponent.vue
+++ bol-management-app/src/components/ListEventsComponent.vue
@@ -12,17 +12,12 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="distribution in Distribution" :key="distribution._id">
+                    <tr v-for="distribution in Distributions" :key="distribution._id">
                         <td>{{ distribution.distribution_id }}</td>
                         <td>{{ distribution.reliefType }}</td>
                         <td>{{ distribution.reliefDate }}</td>
                         <td>{{ distribution.location }}</td>
                         <td>{{ distribution.zip }}</td>
-                        <td>
-                            <router-link :to="{name: 'edit', params: { id: distribution._id }}" class="btn btn-success ">Edit
-                            </router-link>
-                        <button @click.prevent="deleteDistribution(distribution._id)" class="btn btn-danger mx-2">Delete</button>
-                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -48,21 +43,6 @@
             }).catch(error => {
                 console.log(error)
             });
-        },
-        methods: {
-            deleteDistribution(id){
-                console.log(id)
-                let apiURL = `http://localhost:3000/distribution/${id}`;
-                let indexOfArrayItem = this.Distribution.findIndex(i => i._id === id);
-
-                if (window.confirm("Do you really want to delete?")) {
-                    axios.delete(apiURL).then(() => {
-                        this.Distribution.splice(indexOfArrayItem, 1);
-                    }).catch(error => {
-                        console.log(error)
-                    });
-                }
-            }
         }
     }
 </script>
